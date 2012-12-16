ef8e04430331eca8dc1483c49980c3c6da5893a4
Joey Adams <joeyadams3.14159@gmail.com>
2012-12-16 16:09:57 -0500

    Use Network.Mail.SMTP.Abstract to implement the main module
    Note that SMTPConnection no longer holds the initial response lines from
    the server.  We don't need it for anything at the moment.

--- Network/Mail/SMTP.hs
+++ Network/Mail/SMTP.hs
@@ -30,33 +30,34 @@ module Network.Mail.SMTP
     )
     where
 
+import Network.Mail.SMTP.Abstract (MonadSMTP, quit)
+import qualified Network.Mail.SMTP.Abstract as Abstract
 import Network.Mail.SMTP.Auth
 import Network.Mail.SMTP.Types
 
 import System.IO
 import System.FilePath (takeFileName)
 
-import Control.Monad (unless)
-import Data.Monoid
-import Data.Char (isDigit)
+import Control.Exception
+import Control.Monad.IO.Class
+import Data.Word (Word8)
 
 import Network
 import Network.BSD (getHostName)
 import Network.Mail.Mime hiding (simpleMail)
 
 import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.Lazy as B
-import qualified Data.ByteString as S
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
-import Data.Text.Encoding
 
-data SMTPConnection = SMTPC !Handle ![ByteString]
+data SMTPConnection = SMTPC !Handle
 
 instance Eq SMTPConnection where
-    (==) (SMTPC a _) (SMTPC b _) = a == b
+    (==) (SMTPC a) (SMTPC b) = a == b
 
 -- | Connect to an SMTP server with the specified host and port
 connectSMTPPort :: String     -- ^ name of the server
@@ -70,122 +71,27 @@ connectSMTP :: String     -- ^ name of the server
             -> IO SMTPConnection
 connectSMTP = flip connectSMTPPort 25
 
--- | Attemp to send a 'Command' to the SMTP server once
-tryOnce :: SMTPConnection -> Command -> ReplyCode -> IO ByteString
-tryOnce = tryCommand 1
-
--- | Repeatedly attempt to send a 'Command' to the SMTP server
-tryCommand :: Int -> SMTPConnection -> Command -> ReplyCode
-           -> IO ByteString
-tryCommand tries st cmd expectedReply = do
-    (code, msg) <- sendCommand st cmd
-    if code == expectedReply 
-      then return msg
-      else if tries > 1
-        then tryCommand (tries - 1) st cmd expectedReply
-        else do
-          closeSMTP st
-          fail $ "Unexpected reply to: " ++ show cmd ++ 
-            ", Expected reply code: " ++ show expectedReply ++
-            ", Got this instead: " ++ show code ++ " " ++ show msg
-
 -- | Create an 'SMTPConnection' from an already connected Handle
 connectStream :: Handle -> IO SMTPConnection
-connectStream st =
-    do (code1, _) <- parseResponse st
-       unless (code1 == 220) $
-              do hClose st
-                 fail "cannot connect to the server"
-       senderHost <- getHostName
-       msg <- tryCommand 3 (SMTPC st []) (EHLO $ BS.pack senderHost) 250
-       return (SMTPC st (tail $ BS.lines msg))
-
-parseResponse :: Handle -> IO (ReplyCode, ByteString)
-parseResponse st =
-    do (code, bdy) <- readLines
-       return (read $ BS.unpack code, BS.unlines bdy)
-    where readLines =
-              do l <- BS.hGetLine st
-                 let (c, bdy) = BS.span isDigit l
-                 if not (BS.null bdy) && BS.head bdy == '-'
-                    then do (c2, ls) <- readLines
-                            return (c2, BS.tail bdy:ls)
-                    else return (c, [BS.tail bdy])
-
+connectStream h = do
+    let conn = SMTPC h
+    senderHost <- getHostName
+    runSMTP conn $ Abstract.hello senderHost
+    return conn
 
 -- | Send a 'Command' to the SMTP server
 sendCommand :: SMTPConnection -> Command -> IO (ReplyCode, ByteString)
-sendCommand (SMTPC conn _) (DATA dat) =
-    do bsPutCrLf conn "DATA"
-       (code, _) <- parseResponse conn
-       unless (code == 354) $ fail "this server cannot accept any data."
-       mapM_ sendLine $ BS.lines dat ++ [BS.pack "."]
-       parseResponse conn
-    where sendLine = bsPutCrLf conn
-sendCommand (SMTPC conn _) (AUTH LOGIN username password) =
-    do bsPutCrLf conn command
-       _ <- parseResponse conn
-       bsPutCrLf conn userB64
-       _ <- parseResponse conn
-       bsPutCrLf conn passB64
-       (code, msg) <- parseResponse conn
-       unless (code == 235) $ fail "authentication failed."
-       return (code, msg)
-    where command = "AUTH LOGIN"
-          (userB64, passB64) = encodeLogin username password
-sendCommand (SMTPC conn _) (AUTH at username password) =
-    do bsPutCrLf conn command
-       (code, msg) <- parseResponse conn
-       unless (code == 334) $ fail "authentication failed."
-       bsPutCrLf conn $ auth at (BS.unpack msg) username password
-       parseResponse conn
-    where command = BS.pack $ unwords ["AUTH", show at]
-sendCommand (SMTPC conn _) meth =
-    do bsPutCrLf conn command
-       parseResponse conn
-    where command = case meth of
-                      (HELO param) -> "HELO " <> param
-                      (EHLO param) -> "EHLO " <> param
-                      (MAIL param) -> "MAIL FROM:<" <> param <> ">"
-                      (RCPT param) -> "RCPT TO:<" <> param <> ">"
-                      (EXPN param) -> "EXPN " <> param
-                      (VRFY param) -> "VRFY " <> param
-                      (HELP msg)   -> if BS.null msg
-                                        then "HELP\r\n"
-                                        else "HELP " <> msg
-                      NOOP         -> "NOOP"
-                      RSET         -> "RSET"
-                      QUIT         -> "QUIT"
-                      DATA{}       -> 
-                          error "BUG: DATA pattern should be matched by sendCommand patterns"
-                      AUTH{}       ->
-                          error "BUG: AUTH pattern should be matched by sendCommand patterns"
+sendCommand conn = runSMTP conn . Abstract.sendCommand
 
 -- | Send 'QUIT' and close the connection.
 closeSMTP :: SMTPConnection -> IO ()
-closeSMTP c@(SMTPC conn _) = sendCommand c QUIT >> hClose conn
-
--- | Sends a rendered mail to the server. 
-sendRenderedMail :: ByteString   -- ^ sender mail
-            -> [ByteString] -- ^ receivers
-            -> ByteString   -- ^ data
-            -> SMTPConnection
-            -> IO ()
-sendRenderedMail sender receivers dat conn = do
-    _ <- tryOnce conn (MAIL sender) 250
-    mapM_ (\r -> tryOnce conn (RCPT r) 250) receivers
-    _ <- tryOnce conn (DATA dat) 250
-    return ()
+closeSMTP c@(SMTPC conn) = sendCommand c QUIT >> hClose conn
 
 -- | Render a 'Mail' to a 'ByteString' then send it over the specified
 -- 'SMTPConnection'
 renderAndSend ::SMTPConnection -> Mail -> IO ()
-renderAndSend conn mail@Mail{..} = do
-    rendered <- lazyToStrict `fmap` renderMail' mail
-    sendRenderedMail from to rendered conn
-  where enc  = encodeUtf8 . addressEmail 
-        from = enc mailFrom
-        to   = map enc mailTo
+renderAndSend conn mail =
+    runSMTP conn $ Abstract.renderAndSend mail
 
 -- | Connect to an SMTP server, send a 'Mail', then disconnect.
 sendMail :: String -> PortNumber -> Mail -> IO ()
@@ -194,7 +100,7 @@ sendMail host port mail = do
   renderAndSend con mail
   closeSMTP con
 
--- | Connect to an SMTP server, login, send a 'Mail', disconnect.
+-- | Connect to an SMTP server, log in, send a 'Mail', disconnect.
 sendMailWithLogin :: String -> PortNumber -> UserName -> Password -> Mail -> IO ()
 sendMailWithLogin host port user pass mail = do
   con <- connectSMTPPort host port
@@ -239,14 +145,49 @@ filePart :: T.Text -- ^ content type
          -> FilePath -- ^ path to file 
          -> IO Part
 filePart ct fp = do
-    content <- B.readFile fp
+    content <- BL.readFile fp
     return $ Part ct Base64 (Just $ T.pack (takeFileName fp)) [] content 
 
-lazyToStrict :: B.ByteString -> S.ByteString
-lazyToStrict = S.concat . B.toChunks
+------------------------------------------------------------------------
+-- The SMTP monad
 
-crlf :: BS.ByteString
-crlf = BS.pack "\r\n"
+newtype SMTP a = SMTP
+    { runSMTP' :: SMTPConnection -> IO a
+    }
 
-bsPutCrLf :: Handle -> ByteString -> IO ()
-bsPutCrLf h s = BS.hPut h s >> BS.hPut h crlf >> hFlush h
+runSMTP :: SMTPConnection -> SMTP a -> IO a
+runSMTP = flip runSMTP'
+
+instance Monad SMTP where
+    return a = SMTP $ \_r -> return a
+
+    m >>= k = SMTP $ \r -> do
+        a <- runSMTP' m r
+        runSMTP' (k a) r
+
+    fail s = SMTP $ \_r -> fail s
+
+instance MonadIO SMTP where
+    liftIO io = SMTP $ \_r -> io
+
+instance MonadSMTP SMTP where
+    sendLine s = SMTP $ \(SMTPC h) ->
+        B.hPut h s >> B.hPut h "\r\n" >> hFlush h
+
+    recvLine = SMTP $ \(SMTPC h) ->
+        fmap trimCR $ B8.hGetLine h
+
+    throwSMTPError err = do
+        quit
+        SMTP $ \(SMTPC h) -> do
+            hClose h
+            throwIO err
+
+trimCR :: ByteString -> ByteString
+trimCR s = case unsnoc s of
+               Just (s', 13) -> s'
+               _otherwise    -> s
+
+unsnoc :: ByteString -> Maybe (ByteString, Word8)
+unsnoc s | B.null s  = Nothing
+         | otherwise = Just (B.init s, B.last s)
